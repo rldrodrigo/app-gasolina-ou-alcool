0f80635386bba3e2f7fb16f0d03ab49c4e31c4db
Rodrigo Lima <rodrigo.lima1945@gmail.com>
2021-10-12 16:14:22 -0300

    Projeto Finalizado

--- app-gasolina-ou-alcool/src/app/home/home.page.ts
+++ app-gasolina-ou-alcool/src/app/home/home.page.ts
@@ -7,6 +7,33 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
-  constructor() {}
+  constructor() { }
+
+  public resultado: String = "Resultado:"
+  public precoAlcool
+  public precoGasolina
+
+  calcular() {
+    //Validar se os campos foram preenchidos
+    if (this.precoAlcool && this.precoGasolina) {
+      var pAlcool = parseFloat(this.precoAlcool)
+      var pGasolina = parseFloat(this.precoGasolina)
+
+      /* 
+        Faz cálculo (precoAlcool / precoGasolina)
+        se: resultado >= 0.7 melhor utilizar gasolina
+        se não: melhor utilizar alcool
+      */
+      var res = pAlcool / pGasolina
+      if (res >= 0.7) {
+        this.resultado = "Melhor utilizar Gasolina"
+      } else {
+        this.resultado = "Melhor utilizar Álcool"
+      }
+
+    } else {
+      this.resultado = "não preenchidos"
+    }
+  }
 
 }
